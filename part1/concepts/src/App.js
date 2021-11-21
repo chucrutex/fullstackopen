1edffc740fec885cd3927f5cd6bae44b00903cf1
Rafael Menezes <menezes.rafa@gmail.com>
2021-11-21 12:23:17 -0300

    part 1.d Function that returns a function

--- part1/concepts/src/App.js
+++ part1/concepts/src/App.js
@@ -5,13 +5,22 @@ const History = (props) => {
   // o componente mostra um texto de instrução no estado inicial
   if (props.allClicks.length === 0)
     return (
-      <td colSpan="3">the app is used by pressing the buttons</td>
+      <td colSpan="6">the app is used by pressing the buttons</td>
     );
 
   // o componente mostra um texto com os cliques que já foram dados,
   // se não mais estiver no estado inicial
   return (
-    <td colSpan="3">button press history: {props.allClicks.join(" ")}</td>
+    <td colSpan="6">button press history: {props.allClicks.join(" ")}</td>
+  );
+
+};
+
+const MultiValue = (props) => {
+
+  // o componente mostra um texto com o valor clicado dos botões seguintes
+  return (
+    <td colSpan="3">{props.value}</td>
   );
 
 };
@@ -35,6 +44,8 @@ const App = () => {
   // armazena estado de quando direita for clicado
   const [right, setRight] = useState(0);
 
+  const [value, setValue] = useState(10);
+
   // armazena todos os estados de clique, inicializando com um array vazio
   const [allClicks, setAll] = useState([]);
 
@@ -57,6 +68,11 @@ const App = () => {
     setAll([]);
   };
 
+  // ATENÇÃO!!! este trecho retorna uma função
+  const hello = (who) => () => console.log("Hello", who);
+
+  // preenche o estado com o valor recebido
+  const setToValue = (newValue) => setValue(newValue);
 
   // haverá os 2 botões com seus respectivos contadores 
   return (
@@ -66,10 +82,26 @@ const App = () => {
           <tr align="center">
             <Button value={left} onClick={handleLeftClick} text="left" />
             <Button value={right} onClick={handleRightClick} text="right" />
-            <Button value="" onClick={handleResetAll} text="reset" />
+            <Button value="will reset the state of the app" onClick={handleResetAll} text="reset" />
+            <Button value="will say hello world on the console" onClick={hello("world")} text="hello" />
+            <Button value="will say hello react on the console" onClick={hello("react")} text="hello" />
+            <Button value="will say hello function on the console" onClick={hello("function")} text="hello" />
           </tr>
           <tr align="center">
             <History allClicks={allClicks} />
+          </tr>
+        </tbody>
+      </table >
+      <p />
+      <table border="1">
+        <tbody>
+          <tr align="center">
+            <MultiValue value={value} />
+          </tr>
+          <tr align="center">
+            <Button value="will set the value to 1000" onClick={()=>setToValue(1000)} text="thousand" />
+            <Button value="will reset the value" onClick={()=>setToValue(0)} text="reset" />
+            <Button value="will increment the value" onClick={()=>setToValue(value + 1)} text="increment" />
           </tr>
         </tbody>
       </table >
