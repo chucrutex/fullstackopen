92c13eade3eab29280ca2cf49b7067685a4074c6
Rafael Menezes <menezes.rafa@gmail.com>
2021-11-24 16:12:09 -0300

    finished exercise 1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,24 @@
 import React, { useState } from "react";
 
+const TEXT_GOOD = "good";
+const TEXT_NEUTRAL = "neutral";
+const TEXT_BAD = "bad";
+const SPACING = 1;
+
+const handleClick = (buttonPressed, setterFunction, stateValue) => {
+
+  setterFunction(stateValue + 1);
+  console.log("button pressed [", buttonPressed, "]");
+
+};
+
+const Button = (props) => {
+  return (<><button style={{ marginRight: SPACING + "em" }} onClick={props.onClick}>{props.text}</button></>);
+};
+
+const Display = (props) => <>{props.text} {props.value}<br /></>;
+
+const Title = (props) => <><h1>{props.text}</h1></>;
 
 const App = () => {
 
@@ -7,10 +26,16 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
   return (
     <div>
-      code here
+      <Title text="give feedback" />
+      <Button text={TEXT_GOOD} onClick={() => handleClick(TEXT_GOOD, setGood, good)} />
+      <Button text={TEXT_NEUTRAL} onClick={() => handleClick(TEXT_NEUTRAL, setNeutral, neutral)} />
+      <Button text={TEXT_BAD} onClick={() => handleClick(TEXT_BAD, setBad, bad)} />
+      <Title text="statistics" />
+      <Display text={TEXT_GOOD} value={good} />
+      <Display text={TEXT_NEUTRAL} value={neutral} />
+      <Display text={TEXT_BAD} value={bad} />
     </div>
   );
 
