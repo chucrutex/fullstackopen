2a56700ca51cd19f6e3034d530e025c168b7c8e7
Rafael Menezes <menezes.rafa@gmail.com>
2021-11-24 13:34:44 -0300

    starting point exercises 1.6 - 1.14

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,15 +4,16 @@ import React, { useState } from "react";
 const App = () => {
 
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
       code here
     </div>
-  )
+  );
+  
 };
 
 export default App;
