1f031e5b60d56443be399e493eca6275f1363b9e
Rafael Menezes <menezes.rafa@gmail.com>
2021-11-25 12:47:53 -0300

    part 2.a finished topic Map

--- part2/concepts/src/App.js
+++ part2/concepts/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 
 const App = (props) => {
-  
+
   const { notes } = props;
 
-  return (
+  console.log("%cApp.js line:7 notes", "color: #007acc;", notes);
+  let result = notes.map(note => note.content);
+  console.log("%cApp.js line:9 result", "color: #007acc;", result);
+
+  let resultHTML = (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map(note =>
+          <li key={note.id}>
+            {note.content}
+          </li>)}
       </ul>
     </div>
   );
+
+  return resultHTML;
+
 };
 
 export default App;
