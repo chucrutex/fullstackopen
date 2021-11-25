a60350d115c3b154ae1c78d7ff6e63ba7861046b
Rafael Menezes <menezes.rafa@gmail.com>
2021-11-24 21:54:34 -0300

    finished exercise 1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,7 +12,7 @@ const Button = (props) => {
   return (<><button style={{ marginRight: SPACING + "em" }} onClick={props.onClick}>{props.text}</button></>);
 };
 
-const Display = (props) => <>{props.text} {props.value}<br /></>;
+const StatisticsLine = (props) => <>{props.text} {props.value}<br /></>;
 
 const Statistics = ({ good, bad, neutral }) => {
 
@@ -28,12 +28,12 @@ const Statistics = ({ good, bad, neutral }) => {
     result = (<><p>No feedback given</p></>);
   else
     result = (<>
-      <Display text={TEXT_GOOD} value={good} />
-      <Display text={TEXT_NEUTRAL} value={neutral} />
-      <Display text={TEXT_BAD} value={bad} />
-      <Display text={TEXT_ALL} value={all} />
-      <Display text={TEXT_AVERAGE} value={average} />
-      <Display text={TEXT_POSITIVE} value={positive + " %"} />
+      <StatisticsLine text={TEXT_GOOD} value={good} />
+      <StatisticsLine text={TEXT_NEUTRAL} value={neutral} />
+      <StatisticsLine text={TEXT_BAD} value={bad} />
+      <StatisticsLine text={TEXT_ALL} value={all} />
+      <StatisticsLine text={TEXT_AVERAGE} value={average} />
+      <StatisticsLine text={TEXT_POSITIVE} value={positive + " %"} />
     </>);
 
   return result;
