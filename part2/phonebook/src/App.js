2b40412080cddfd9f04f2d9d23be7a091fbb18d2
Rafael Menezes <menezes.rafa@gmail.com>
2021-12-02 17:35:46 -0300

    finished exercise 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,28 @@ import React, { useState } from "react"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
+    { name: "Arto Hellas", number: "040-123456" }
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const validateFields = (newName, newNumber) => {
+
+    // não deixa adicionar nome vazio
+    if (newName.trim && newName.trim().length <= 0) {
+      alert("Name must be filled");
+      return false;
+    }
+
+    // não deixa adicionar número vazio
+    if (newNumber.trim && newNumber.trim().length <= 0) {
+      alert("Number must be filled");
+      return false;
+    }
+
+    return true;
+
+  };
 
   // tratador do botão de adicionar nome
   const handleNewNameClick = (event) => {
@@ -12,26 +31,26 @@ const App = () => {
     // impede refresh da página
     event.preventDefault();
 
-    // não deixa adicionar nome vazio
-    if (newName.trim && newName.trim().length <= 0)
-      alert("Name must be filled");
+    // não deixa adicionar campos vazios
+    if (!validateFields(newName, newNumber))
+      return;
 
-    // se chegou aqui, é pq o nome da pessoa foi preenchido
+    // se chegou aqui, é pq os campos estão válidos
     console.log("%cConteúdo da variável event", "color: #007acc;", event);
 
+    // arruma os espaços sobressalentes
     let newNameTrimmed = newName.trim();
+    let newNumberTrimmed = newNumber.trim();
 
     if (persons.find(person => person.name === newNameTrimmed))
       alert(newNameTrimmed + " is already added to phonebook");
     else {
 
       // adiciona o nome ao array
-      let newPerson = { name: newNameTrimmed };
+      let newPerson = { name: newNameTrimmed, number: newNumberTrimmed };
       setPersons(persons.concat(newPerson));
-
-      // limpa o campo do nome
       setNewName("");
-
+      setNewNumber("");
     }
 
   };
@@ -42,19 +61,22 @@ const App = () => {
     setNewName(event.target.value);
   };
 
+  const handleChangeNewNumber = (event) => {
+    console.log("%cConteúdo da variável event.target.value", "color: #007acc;", event.target.value);
+    setNewNumber(event.target.value);
+  };
+
   return (
+
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input onChange={handleChangeNewName} value={newName} />
-        </div>
-        <div>
-          <button type="submit" onClick={handleNewNameClick}>add</button>
-        </div>
+        <div>name: <input onChange={handleChangeNewName} value={newName} /></div>
+        <div>number: <input onChange={handleChangeNewNumber} value={newNumber} /></div>
+        <div><button type="submit" onClick={handleNewNameClick}>add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}<br /></div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}<br /></div>)}
     </div>
   );
 };
