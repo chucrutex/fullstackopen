138e1158fd09fd4dbd7426f53605931c5daf3767
Rafael Menezes <menezes.rafa@gmail.com>
2021-12-02 13:12:10 -0300

    finished 2.b Filtering Displayed Elements

--- part2/concepts/src/App.js
+++ part2/concepts/src/App.js
@@ -5,6 +5,9 @@ const App = (props) => {
 
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note");
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
 
   const addNote = (event) => {
 
@@ -21,6 +24,8 @@ const App = (props) => {
       id: notes.length + 1
     };
 
+    console.log("%cConteúdo da variável noteObject", "color: #007acc;", noteObject);
+
     // preenche o estado que representa as notas
     setNotes(notes.concat(noteObject));
 
@@ -30,15 +35,19 @@ const App = (props) => {
   };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
   let resultHTML = (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
