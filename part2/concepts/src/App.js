82fb73c2bbd6f59132512139879ae8c0b4b8106d
Rafael Menezes <menezes.rafa@gmail.com>
2021-12-01 23:39:56 -0300

    add note event added

--- part2/concepts/src/App.js
+++ part2/concepts/src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note");
+
+  const addNote = (event) => {
+
+    // impede recarga da página no submit
+    event.preventDefault();
+    console.log("button clicked", event.target);
+
+    // cria um objeto nota
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      // 50% de chances da nota ser criada como importante
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    };
+
+    // preenche o estado que representa as notas
+    setNotes(notes.concat(noteObject));
+
+    // limpa o campo de nota nova
+    setNewNote("");
+
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   let resultHTML = (
     <div>
@@ -9,8 +40,11 @@ const App = ({ notes }) => {
       <ul>
         {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
-    </div>
-  );
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>);
 
   return resultHTML;
 
